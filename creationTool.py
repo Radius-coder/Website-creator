ec09435029618488036ad3e470ac10bedf64cbb1
Darius <67561957+Radius-coder@users.noreply.github.com>
2020-07-10 00:41:21 +0100

    Add files via upload
    Added a main menu with more customisation options. Still need to fix the centered navigation bar, add options to curve the different containers. add options for video containers, add option for slideshows, add option to delete divs and ids, including social media logos and links, maybe a login form. finally a option to open the file after completeion.

--- creationTool.py
+++ creationTool.py
@@ -2,7 +2,7 @@
 #can be  used to save time creating allot of code based on yes or no answers
 #e.g do you want a header? answer 'yes' or 'no'
 #print code to file and save with relatable tag
-
+#add gui with buttons and pictures to show what it will look like
 #file has to be created before it can be edited!
 
 import sys
@@ -11,10 +11,11 @@ html = html.replace('.html', '')
 css = input("\nName css file: ")
 css = css.replace('.css', '')
 original_stdout = sys.stdout
+i = 0
 
 mainloop = 1
 while mainloop == 1:
-
+    print("File has to be created before it can be edited.\n")
     mode = int(input("Do you want to 1: create 2: edit?\n"))
     if mode == 1:
     #create html ready to be edited
@@ -26,13 +27,13 @@ while mainloop == 1:
             <HEAD>
             <link rel="stylesheet" type="text/css" href="'''+css +'''.css">
             </HEAD>
-    
+
             <BODY>
-     
+
             ''')
             sys.stdout = original_stdout #redirects output to python
             print("completed html")
-    
+
         #create css ready to be edited
         file = open(css + '.css', 'w')
         with file as fw:
@@ -42,10 +43,10 @@ while mainloop == 1:
             sys.stdout = original_stdout #redirects output to python
             print("completed css\n")
         mainloop = 1
-       
+
 
     elif mode == 2:
-               
+
         choice = int(input("Would you like to:\n1. Add Header, logo and title\n2. Add navigation bar\n3. Add Images\n4. Add Text columns\n5. Add Footer\n"))
         if choice == 1:
         #question 1
@@ -54,9 +55,9 @@ while mainloop == 1:
             while loop ==1:
                 print("Hello, would you like to create a header with space for a logo and title. Answer with 1 or 2?\n")
                 answer = int(input("1) YES\n2) NO\n"))
-        
+
                 if answer == 1:
-                    name = input("First, name this header: ")
+                    name = input("First, name this header container: ")
                     title = input("What would you like to write in the header: ")
                     text = input("What color would you like the text to be: ")
         #creation of html file
@@ -65,7 +66,7 @@ while mainloop == 1:
                         sys.stdout = fw # direct the standard output to the file
                         print('''
                 <div class="'''+name+'''">
-                <h1 style="color-'''+text+'''">''',title,'''</h1>
+                <h1 style="color:'''+text+'''">''',title,'''</h1>
                 </div>
                 ''')
                     sys.stdout = original_stdout #redirects output to python
@@ -73,11 +74,11 @@ while mainloop == 1:
 
                     image = input("Enter name of jpg logo: ")
                     bg = input("What color background do you want the header: ")
-                    
-                    
-                    
-                    
-                
+
+
+
+
+
         #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
@@ -96,7 +97,7 @@ while mainloop == 1:
                     sys.stdout = original_stdout #redirects output to python
                     print("completed css\n")
                     loop = 0
-            
+
                 elif answer == 2:
                     print("You have selected NO\n")
                     loop = 0
@@ -108,6 +109,7 @@ while mainloop == 1:
 #================================================================================================================
     #question 2
     #start of my if loops
+        
             loop = 1
             while loop ==1:
                 original_stdout = sys.stdout
@@ -115,14 +117,17 @@ while mainloop == 1:
                 answer = int(input("1) TOP\n2) LEFT\n3) NONE\n"))
 
                 if answer == 1:
+                    
+                    
+                    navName = input("First, name the navigation bar container: ")
                     linkAmount = int(input("How many links would you like to include: "))
                     #creation of html file
                     file = open(html + '.html', 'a')
                     with file as fw:
                         sys.stdout = fw # direct the standard output to the file
-                        print('''<div class="topnav">''')
+                        print('''<div class="''',navName+'''">''')
                         sys.stdout = original_stdout #redirects output to python
-                        i=0         
+                        
                         while i<linkAmount:
                             link = input("Enter name of other html page you wish to link: ")
                             linkName = input("What will the nav bar say for this link: ")
@@ -130,46 +135,58 @@ while mainloop == 1:
                             print("<a href='",link+".html'>",linkName,"</a>")
                             sys.stdout = original_stdout
                             i+=1
-                        
+
                         sys.stdout = fw
                         print("</div>")
-                            
+
                         sys.stdout = original_stdout #redirects output to python
                         print("completed html")
 
-                        color = input("What color would you lke the navigation bar to be: ")
+                        bgcolor = input("What color would you lke the navigation bar to be: ")
                         text = input("What color should the text be: ")
                         hover = input("What color should the link appear when the user is hovering over it: ")
                         textHover = input("What color should the text turn when user is hovering over it: ")
+                        linkPos = ""
+                        linkPosCheck = 1
+                        while linkPosCheck == 1:
+                            linkPos = input("Do you want the links on the:\n1. Left\n2. Center\n3. Right\n")
+                            if linkPos == "1":
+                                linkPos = "float: left;\npadding: 14px 16px;\ntext-decoration: none;\nfont-size: 17px;\ntext-align: center;"
+                                linkPosCheck = 0
+                            elif linkPos == "2":
+                                linkPos = "float: none;\nposition: absolute;\ntop: 5%;\nleft: 50%;\ntransform: translate(-50%, -50%);\nfont-size: 17px;\ntext-align: center;"
+                                linkPosCheck = 0
+                            elif linkPos == "3":
+                                linkPos = "float: right;\ndisplay: block\nfont-size: 17px;\ntext-align: center;\npadding: 14px 16px;\ntext-decoration: none;"
+                                linkPosCheck = 0
+                            else:
+                                print("Invalid Choice!\n")
+                                linkPosCheck = 1
+                        
+                                
             #creation of css file
                         file = open(css + '.css', 'a')
                         with file as fw:
                             sys.stdout = fw # direct the standard output to the file
-                            print('''.topnav{
-                        overflow: hidden;
-                        background-color:''',color,''';
-                        border-style: solid;
-                        border-width: 1px;
-                        border-radius: 3px 3px 3px 3px;
-                }
-                /*nav bar links*/
-                .topnav a{
-                        float: right;
-                        display: block;
-                        color: ''',text,''';
-                        text-align: center;
-                        padding: 14px 16px;
-                        text-decoration: none;
-                        border-style: solid;
-                        border-width: 1px;
-                }
-                /*nav hover color*/
-                .topnav a:hover{
-                        background-color:''',hover,''';
-                        color:''',textHover,''';
-                }
-                    
-                    ''')
+                            print('''.'''+navName+''' {
+                                        overflow: hidden;
+                                        background-color: '''+bgcolor+''';
+                        
+                                        }
+                                        /*nav bar links*/
+                                        .'''+navName+''' a{
+                                                ''', linkPos+'''
+                                                color: ''',text+''';
+                                                text-align: center;"
+                                                
+                                        }
+                                        /*nav hover color*/
+                                        .'''+navName+''' a:hover{
+                                                background-color: '''+hover+''';
+                                                color: '''+textHover+''';
+                                        }
+
+                                    ''')
                         sys.stdout = original_stdout #redirects output to python
                         print("completed css")
                         loop = 0
@@ -198,15 +215,41 @@ while mainloop == 1:
                 if answer == 1:
                     name = input("First, name the image container: ")
                     image = input("Enter name of jpg image: ")
+                    height = input("What height do you want the image to be. e.g.500px,  550px for full page: ")
+                    width = input("What width do you want the image to be. e.g.1000px, 1355px for full page: ")
                     head = input("Enter image header text: ")
                     subHead = input("Enter image sub header text: ")
                     color = input("What color would you like the text in image: ")
+                    textPosition = input("Would you like the text to be:\n1. Centered\n2. Bottom left\n3. Bottom right\n4. Top left\n5. Top right\n")
+                    textPosLoop = 1
+                    while textPosLoop == 1:
+                        if textPosition == "1":
+                            textPosition = "top: 50%;\nleft: 50%;"
+                            textPosLoop = 0
+                        elif textPosition == "2":
+                            textPosition = "top: 70%;\nleft: 20%;"
+                            textPosLoop = 0
+                        elif textPosition == "3":
+                            textPosition = "top: 70%;\nleft: 70%;"
+                            textPosLoop = 0
+                        elif textPosition == "4":
+                            textPosition = "top: 20%;\nleft: 20%;"
+                            textPosLoop = 0
+                        elif textPosition == "5":
+                            textPosition = "top: 20%;\nleft: 70%;"
+                            textPosLoop = 0
+                        else:
+                            print("Invalid Answer!\n")
+                            textPosition = input("Would you like the text to be:\n1. Centered\n2. Bottom left\n3. Bottom right\n4. Top left\n5. Top right\n")
+                    
+                            textPosLoop = 1
+                            
             #creation of html file
                     file = open(html + '.html', 'a')
                     with file as fw:
                         sys.stdout = fw # direct the standard output to the file
                         print('''<div class="'''+name+'''">
-             <div class="blog-text">
+             <div class="'''+name+'''-text">
              <h1 style="font-size:50px; color:''',color+'''">''',head+'''</h1>
              <h3 style="color:''',color+'''">''',subHead+'''</h3>
              </div>
@@ -214,29 +257,28 @@ while mainloop == 1:
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed html")
-                    
+
             #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
                         sys.stdout = fw # direct the standard output to the file
                         print('''.'''+name+'''{
                     float: center;
-            background: url(''',image+'''.jpg) no-repeat center;
+            background: url('''+image+'''.jpg) no-repeat center;
             background-size: cover;
-            height: 500px;
-            width: 90%;
+            height:''',height+''';
+            width: ''',width+''';
             margin: auto;
             position: relative;
-            border-radius: 0px 0px 30px 30px;
             }
-            .blog-text{
+            .'''+name+'''-text{
             text-align: center;
             position: absolute;
-            top: 50%;
-            left: 50%;
+            
+            '''+textPosition+'''
             transform: translate(-50%, -50%);
             }
-                
+
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed css\n")
@@ -257,11 +299,11 @@ while mainloop == 1:
               <img src="''',image2+'''.jpg" alt="sideimg1" >
               </div>
             </div>
-              
+
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed html")
-                    
+
                     #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
@@ -270,7 +312,7 @@ while mainloop == 1:
               float: left;
               width: 50%;
               padding: 0px;
-              
+
             }
             .clearfix::after {
               content: "";
@@ -286,7 +328,7 @@ while mainloop == 1:
                     image = input("Enter first jpg image name: ")
                     image2= input("Enter second jpg image name: ")
                     image3= input("Enter third jpg image name: ")
-                    
+
                     #creation of html file
                     file = open(html + '.html', 'a')
                     with file as fw:
@@ -295,21 +337,21 @@ while mainloop == 1:
               <div class="img-container">
               <img src="''',image + '''.jpg" alt="sideimg1" style="width:100%">
               </div>
-              
+
               <div class="img-container">
               <img src="''',image2 + '''.jpg" alt="sideimg2" style="width:100%">
               </div>
-              
+
               <div class="img-container">
               <img src="''',image3 + '''.jpg" alt="sideimg3" style="width:100%">
               </div>
             </div>
             </br>
-              
+
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed html")
-                    
+
                     #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
@@ -318,7 +360,7 @@ while mainloop == 1:
             .img-container {
               float: left;
               width: 33.33%;
-              
+
             }
             .clearfix::after {
               content: "";
@@ -329,7 +371,7 @@ while mainloop == 1:
                         sys.stdout = original_stdout #redirects output to python
                     print("completed css\n")
                     loop = 0
-                    
+
                 elif answer == 4:
                     print("You have selected NO\n")
                     loop = 0
@@ -379,7 +421,7 @@ while mainloop == 1:
               width: 99%;
               border: 10px solid transparent;
                     border-image: url(''',borderImg+'''.jpg) 30% round;
-              
+
             }
             /*Responsive layout columns */
             @media screen and (max-width: 600px) {
@@ -387,7 +429,7 @@ while mainloop == 1:
                 width: 100%;
               }
             }
-                
+
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed css\n")
@@ -416,7 +458,7 @@ while mainloop == 1:
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed html")
-                    
+
             #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
@@ -455,7 +497,7 @@ while mainloop == 1:
                 ''')
                     sys.stdout = original_stdout #redirects output to python
                     print("completed html")
-                    
+
             #creation of css file
                     file = open(css + '.css', 'a')
                     with file as fw:
@@ -482,7 +524,7 @@ while mainloop == 1:
                     print("Invalid answer. Enter 1 for yes or 2 for no.\n")
                     loop = 1
 
-    
+
         elif choice == 5:
     #question 5
             #start of my if loops
@@ -512,12 +554,12 @@ while mainloop == 1:
                     with file as fw:
                         sys.stdout = fw # direct the standard output to the file
                         print('''.footer{
-	    
+
 	    background-color: ''',bgColor+''';
 	    text-align: center;
 	    padding: 10px;
-	    
-	    
+
+
     }
     ''')
                     sys.stdout = original_stdout #redirects output to python
@@ -531,6 +573,6 @@ while mainloop == 1:
                 else:
                     print("Invalid answer. Enter 1 for yes or 2 for no.\n")
                     loop = 1
-                        
+
     else:
             loop = 1
